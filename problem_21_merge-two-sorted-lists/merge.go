2d441d5c0bcbb7b2f87d73a1f232e5b4dedbe91f
Myron Meng <dancemystyle@gmail.com>
2019-12-24 23:20:50 +0800

    [feat]: 添加合并两个有序链表

---
+++ problem_21_merge-two-sorted-lists/merge.go
@@ -0,0 +1,33 @@
+package problem_21_merge_two_sorted_lists
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 时间复杂度：O(m+n)，其中m,n分别为两个链表的长度
+// 空间复杂度：O(1)
+func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	preHead := &ListNode{}
+	prev := preHead
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			prev.Next = l1
+			l1 = l1.Next
+		} else {
+			prev.Next = l2
+			l2 = l2.Next
+		}
+		prev = prev.Next
+	}
+
+	if l1 != nil {
+		prev.Next = l1
+	} else {
+		prev.Next = l2
+	}
+
+	return preHead.Next
+}
