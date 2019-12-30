d9e78dc6e9086be225c7592f469e7c115a609adb
Myron Meng <dancemystyle@gmail.com>
2019-12-30 12:31:09 +0800

    [feat]: 解答两数之和

---
+++ problem_1_two-sum/main.go
@@ -0,0 +1,13 @@
+package problem_1_two_sum
+
+func TwoSum(nums []int, target int) []int {
+	m := make(map[int]int, 0)
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+		if cpm, ok := m[complement]; ok {
+			return []int{cpm, i}
+		}
+		m[nums[i]] = i
+	}
+	return []int{}
+}
