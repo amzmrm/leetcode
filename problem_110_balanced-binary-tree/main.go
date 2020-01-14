b926012b8d435706b99231a7a34c8965b4ed613d
Myron Meng <dancemystyle@gmail.com>
2020-01-14 10:49:44 +0800

    [feat]: 完善平衡二叉树

--- problem_110_balanced-binary-tree/main.go
+++ problem_110_balanced-binary-tree/main.go
@@ -15,20 +15,20 @@ func IsBalanced(root *TreeNode) bool {
 		return true
 	}
 
-	return depth(root) != -1
+	return height(root) != -1
 }
 
-func depth(node *TreeNode) int {
+func height(node *TreeNode) int {
 	if node == nil {
 		return 0
 	}
 
-	left := depth(node.Left)
+	left := height(node.Left)
 	if left == -1 {
 		return -1
 	}
 
-	right := depth(node.Right)
+	right := height(node.Right)
 	if right == -1 {
 		return -1
 	}
