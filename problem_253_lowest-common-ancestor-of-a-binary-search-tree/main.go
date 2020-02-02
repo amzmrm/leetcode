82bb40dcd94f0faaf30fb6240ab1b29c53420d85
Myron Meng <dancemystyle@gmail.com>
2020-02-02 22:06:28 +0800

    [fix]: 解决二叉搜索树的最近公共祖先添加空间复杂度O(1)解法。

--- problem_253_lowest-common-ancestor-of-a-binary-search-tree/main.go
+++ problem_253_lowest-common-ancestor-of-a-binary-search-tree/main.go
@@ -16,20 +16,39 @@ func LowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 		min, max = max, min
 	}
 
-	return helper(root, min, max)
+	return helper2(root, min, max)
 }
 
 // 递归找出第一个值在[min,max)之间的节点
 // 时间复杂度：O(N)
 // 空间复杂度：O(N)
-func helper(root *TreeNode, min int, max int) *TreeNode {
+func helper1(root *TreeNode, min int, max int) *TreeNode {
 	if max < root.Val {
-		return helper(root.Left, min, max)
+		return helper1(root.Left, min, max)
 	}
 
 	if root.Val < min {
-		return helper(root.Right, min, max)
+		return helper1(root.Right, min, max)
 	}
 
 	return root
+}
+
+// 迭代找出第一个值在[min,max)之间的节点
+// 时间复杂度：O(N)
+// 空间复杂度：O(1)
+func helper2(root *TreeNode, min int, max int) *TreeNode {
+	node := root
+	for node != nil {
+		if max < node.Val {
+			node = node.Left
+			continue
+		}
+		if node.Val < min {
+			node = node.Right
+			continue
+		}
+		return node
+	}
+	return nil
 }
