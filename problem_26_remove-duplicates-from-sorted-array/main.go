913a58b8aa5d00247868b1f751bdcb33d19bdfde
Myron Meng <dancemystyle@gmail.com>
2020-02-05 15:36:06 +0800

    [feat]: 解决删除排序数组中的重复项。

---
+++ problem_26_remove-duplicates-from-sorted-array/main.go
@@ -0,0 +1,42 @@
+package problem_26_remove_duplicates_from_sorted_array
+
+func RemoveDuplicates(nums []int) int {
+	originalLen := len(nums)
+	if originalLen <= 1 {
+		return originalLen
+	}
+
+	prev := nums[0]
+
+	// 记录删除的数字个数
+	j := 0
+	for i := 1; i < len(nums); {
+		if i+j >= originalLen {
+			break
+		}
+
+		if nums[i] == prev {
+			nums = append(nums[:i], nums[i+1:]...)
+			j++
+		} else {
+			prev = nums[i]
+			i++
+		}
+	}
+	return len(nums)
+}
+
+func RemoveDuplicates2(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
