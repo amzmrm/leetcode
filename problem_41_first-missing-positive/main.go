897763502c820be65d89d585cb06a459696c6909
Myron Meng <dancemystyle@gmail.com>
2020-02-13 20:11:37 +0800

    [feat]: 解决缺失的第一个整数。

---
+++ problem_41_first-missing-positive/main.go
@@ -0,0 +1,70 @@
+package problem_41_first_missing_positive
+
+import "math"
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+func firstMissingPositive(nums []int) int {
+	length := len(nums)
+
+	// 空切片，返回1
+	if length == 0 {
+		return 1
+	}
+
+	count := 0
+	// 检查 1 是否存在于数组中
+	// O(n)
+	for _, num := range nums {
+		if num == 1 {
+			count++
+		}
+	}
+
+	// 如果nums没有1，那么缺失的第一个正整数就是1
+	if count == 0 {
+		return 1
+	}
+
+	// nums包含有1，但是length为1，那么缺失的第一个正整数就是2
+	if length == 1 {
+		return 2
+	}
+
+	// 将负数和大于length的数替换为1
+	// O(n)
+	for i, num := range nums {
+		if num <= 0 || num > length {
+			nums[i] = 1
+		}
+	}
+
+	// 遍历数组。当读到数字a时，替换第a个元素的符号
+	// O(n)
+	a := 0
+	for i := 0; i < length; i++ {
+		a = int(math.Abs(float64(nums[i])))
+		if a == length {
+			nums[0] = - int(math.Abs(float64(nums[0])))
+		} else {
+			nums[a] = - int(math.Abs(float64(nums[a])))
+		}
+	}
+
+	// 再次遍历数组。返回第一个正数元素的下标
+	// O(n)
+	for i := 1; i < length; i++ {
+		if nums[i] > 0 {
+			return i
+		}
+	}
+
+	// 如果nums[0] > 0，则返回length。这种情况是缺失的第一个正整数就是数组的长度
+	if nums[0] > 0 {
+		return length
+	}
+
+	// 如果之前的步骤中没有发现nums中有正数元素，则返回n + 1
+	// 这种情况是数组刚好包含了length个不同的正整数，缺失的第一个正整数就是length+1
+	return length + 1
+}
---
+++ problem_41_first-missing-positive/main_test.go
@@ -0,0 +1,51 @@
+package problem_41_first_missing_positive
+
+import "testing"
+
+func TestFirstMissingPositive(t *testing.T) {
+	type test struct {
+		name   string
+		nums   []int
+		expect int
+	}
+
+	tests := make([]*test, 0)
+
+	list1 := []int{1, 2, 0}
+	list2 := []int{1, 2, 3, 4, 5, 6}
+	list3 := []int{3, 4, -1, 1}
+	list4 := []int{7, 8, 9, 11, 12}
+
+	tests = append(tests, &test{
+		name:   "list1",
+		nums:   list1,
+		expect: 3,
+	})
+
+	tests = append(tests, &test{
+		name:   "list2",
+		nums:   list2,
+		expect: 7,
+	})
+
+	tests = append(tests, &test{
+		name:   "list3",
+		nums:   list3,
+		expect: 2,
+	})
+
+	tests = append(tests, &test{
+		name:   "list4",
+		nums:   list4,
+		expect: 1,
+	})
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := FirstMissingPositive(test.nums)
+			if got != test.expect {
+				t.Fatalf("got:%v, expect:%v", got, test.expect)
+			}
+		})
+	}
+}
