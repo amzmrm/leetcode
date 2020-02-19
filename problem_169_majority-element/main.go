d5067a08530478adcffebf816dc1b7b389a60f06
Myron Meng <dancemystyle@gmail.com>
2020-02-19 09:27:22 +0800

    [feat]:求众数添加新解法。

--- problem_169_majority-element/main.go
+++ problem_169_majority-element/main.go
@@ -25,3 +25,21 @@ func majorityElement(nums []int) int {
 
 	return count
 }
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+func majorityElement2(nums []int) int {
+	count := 0
+	candidate := 0
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+		if num == candidate {
+			count += 1
+		} else {
+			count += -1
+		}
+	}
+	return candidate
+}
