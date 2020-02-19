ff12c86d2fbc1e1da26b96c9ad56306e34691b33
Myron Meng <dancemystyle@gmail.com>
2020-02-20 00:25:58 +0800

    [feat]:解决爬楼梯。

---
+++ problem_70_climbing-stairs/main.go
@@ -0,0 +1,20 @@
+package problem_70_climbing_stairs
+
+func ClimbStairs(n int) int {
+	if n <= 0 {
+		return 0
+	}
+
+	results := []int{0, 1, 2}
+	if n <= 2 {
+		return results[n]
+	}
+
+	stairOne, stairTwo, stairN := 2, 1, 0
+	for i := 3; i <= n; i++ {
+		stairN = stairOne + stairTwo
+		stairTwo, stairOne = stairOne, stairN
+	}
+
+	return stairN
+}
