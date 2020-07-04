a274ddc5441f5f48620b334cb48a7307472c4005
Boren Meng <boren@runtheworld.today>
2020-07-04 12:31:49 +0800

    Solve problem 235: lowest common ancestor of a binary search tree

---
+++ problem_235_lowest-common-ancestor-of-a-binary-search-tree/main.go
@@ -0,0 +1,33 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func LowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	min, max := p.Val, q.Val
+	if q.Val < min {
+		min, max = max, min
+	}
+
+	return helper(root, min, max)
+}
+
+// 递归找出第一个值在[min,max)之间的节点
+func helper(root *TreeNode, min int, max int) *TreeNode {
+	if max < root.Val {
+		return helper(root.Left, min, max)
+	}
+
+	if root.Val < min {
+		return helper(root.Right, min, max)
+	}
+
+	return root
+}
