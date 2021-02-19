f32d1d2be25d8a0505eafd857f284a53323910b0
Boren Meng <boren@runtheworld.today>
2021-02-19 21:08:57 +0800

    Problem 105 solved.

---
+++ 105_construct-binary-tree-from-preorder-and-inorder-traversal/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestBuildTree(t *testing.T) {
+	preorder := []int{3, 9, 20, 15, 7}
+	inorder := []int{9, 3, 15, 20, 7}
+
+	got := buildTree(preorder, inorder)
+	if got.Val != 3 {
+		t.Fatalf("expect: 3, got: %d", got.Val)
+	}
+}
