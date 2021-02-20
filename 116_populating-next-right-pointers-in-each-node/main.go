6461f767c3dbac910e4a9ea289202f38d18791fc
Boren Meng <boren@runtheworld.today>
2021-02-20 22:23:11 +0800

    Problem 116 solved.

---
+++ 116_populating-next-right-pointers-in-each-node/main.go
@@ -0,0 +1,31 @@
+package leetcode
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	connectTwoNodes(root.Left, root.Right)
+
+	return root
+}
+
+func connectTwoNodes(node1 *Node, node2 *Node) {
+	if node1 == nil || node2 == nil {
+		return
+	}
+
+	node1.Next = node2
+
+	connectTwoNodes(node1.Left, node1.Right)
+	connectTwoNodes(node2.Left, node2.Right)
+
+	connectTwoNodes(node1.Right, node2.Left)
+}
