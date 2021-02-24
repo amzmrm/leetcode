5ef168d60446c36124da0dd7987d95beb415aae8
Myron Meng <dancemystyle@gmail.com>
2021-02-24 08:27:21 -0600

    Problem 1038 solved.

---
+++ 1038_binary-search-tree-to-greater-sum-tree/main.go
@@ -0,0 +1,29 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func bstToGst(root *TreeNode) *TreeNode {
+	var sum int
+
+	var traverse func(*TreeNode)
+	traverse = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		traverse(node.Right)
+
+		sum += node.Val
+
+		node.Val = sum
+		traverse(node.Left)
+	}
+
+	traverse(root)
+
+	return root
+}
