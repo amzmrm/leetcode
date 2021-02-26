3088f9cb2cf5a47b362d0fd3a38c9da196f72c41
Myron Meng <dancemystyle@gmail.com>
2021-02-26 08:36:33 -0600

    Problem 98 solved again.

--- 98_validate-binary-search-tree/main.go
+++ 98_validate-binary-search-tree/main.go
@@ -72,3 +72,20 @@ func IsValidBST(root *TreeNode) bool {
 
 	return true
 }
+
+func IsIsValidBSTV2(root *TreeNode) bool {
+	return IsValidBSTHelpV2(root, nil, nil)
+}
+
+func IsValidBSTHelpV2(root, min, max *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if min != nil && root.Val <= min.Val {
+		return false
+	}
+	if max != nil && root.Val >= max.Val {
+		return false
+	}
+	return IsValidBSTHelpV2(root.Left, min, root) && IsValidBSTHelpV2(root.Right, root, max)
+}
