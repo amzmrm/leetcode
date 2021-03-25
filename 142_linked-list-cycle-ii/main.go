f1451fa09c3d96e5df033795faeaa91e8036a10c
Myron Meng <dancemystyle@gmail.com>
2021-03-25 09:15:09 +0800

    Problem 142:linked-list-cycle-ii solved.

---
+++ 142_linked-list-cycle-ii/main.go
@@ -0,0 +1,33 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+
+	var hasCycle bool
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			hasCycle = true
+			break
+		}
+	}
+	if !hasCycle {
+		return nil
+	}
+
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
