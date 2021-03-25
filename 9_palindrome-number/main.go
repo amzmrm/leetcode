b1c3cfee4fbf5c38d642082c86a7ce72903b6916
Myron Meng <dancemystyle@gmail.com>
2021-03-25 14:21:04 +0800

    Optimize 9:palindrome-number solved.

---
+++ 9_palindrome-number/main.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x != 0 && x%10 == 0) {
+		return false
+	}
+
+	reverted := 0
+	for x > reverted {
+		reverted = reverted*10 + x%10
+		x = x / 10
+	}
+	return x == reverted || x == reverted/10
+}
