a63181b896016a139786c8334f7161ff8d2df865
Myron Meng <dancemystyle@gmail.com>
2021-03-29 08:07:42 +0800

    Optimize 56:merge-intervals.

--- 56_merge-intervals/main.go
+++ 56_merge-intervals/main.go
@@ -16,13 +16,16 @@ func Merge(intervals [][]int) [][]int {
 	merged = append(merged, intervals[0])
 	for i := 1; i < len(intervals); i++ {
 		if intervals[i][0] > merged[len(merged)-1][1] {
+			// 区间的开始坐标大于最后一个已合并区间的结束坐标，表示这两个区间没有重叠
 			merged = append(merged, intervals[i])
 			continue
-		}
-		if merged[len(merged)-1][1] < intervals[i][1] {
-			merged[len(merged)-1][1] = intervals[i][1]
+		} else if intervals[i][0] <= merged[len(merged)-1][1] {
+			// 区间的开始坐标小于或等于最后一个已合并区间的结束坐标，表示这两个区间有重叠
+			if intervals[i][1] > merged[len(merged)-1][1] {
+				// 区间的结束坐标大于最后一个已合并区间的结束坐标，表示要合并
+				merged[len(merged)-1][1] = intervals[i][1]
+			}
 		}
 	}
-
 	return merged
 }
